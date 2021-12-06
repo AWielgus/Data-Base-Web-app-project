aebe62561a987a3dff3ae933c1c0c8838d1c696c
Adrian Wielgus <adrianwielgus99@gmail.com>
2021-12-06 13:14:24 +0100

    spring initializer

---
+++ src/main/java/com/AW/DataBaseWeb/DataBaseWebApplication.java
@@ -0,0 +1,13 @@
+package com.AW.DataBaseWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AW/DataBaseWeb/DataBaseWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AW.DataBaseWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
