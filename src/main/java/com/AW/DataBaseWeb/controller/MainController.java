6aeb20ac3d6f698874b83fd8fcacf470d6c5e9b6
AWielgus <adrianwielgus99@gmail.com>
2022-02-07 23:21:52 +0100

    prepared front for function and procedures

--- src/main/java/com/AW/DataBaseWeb/controller/MainController.java
+++ src/main/java/com/AW/DataBaseWeb/controller/MainController.java
@@ -2,6 +2,8 @@ package com.AW.DataBaseWeb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -9,6 +11,13 @@ public class MainController {
     @GetMapping("/")
     public String index(){
         return "home";
+    }
+
+//    procedures
+    @PostMapping("/proc1")
+    public String test(@ModelAttribute("value") String value){
+        System.out.println(value);
+        return "redirect:/";
     }
 
 }
