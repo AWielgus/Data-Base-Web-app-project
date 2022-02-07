a8ca66f7718606b0eb8f3b4d023fef2a3b1dc2e8
AWielgus <adrianwielgus99@gmail.com>
2022-02-07 22:10:29 +0100

    fix

--- src/main/java/com/AW/DataBaseWeb/repository/ReviewRepository.java
+++ src/main/java/com/AW/DataBaseWeb/repository/ReviewRepository.java
@@ -1,7 +1,6 @@
 package com.AW.DataBaseWeb.repository;
 
 import com.AW.DataBaseWeb.model.Review;
-import com.AW.DataBaseWeb.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,5 +9,5 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
     @Query("select a from Review a where a.hidden=false ")
-    List<Tag> findAllNotHidden();
+    List<Review> findAllNotHidden();
 }
